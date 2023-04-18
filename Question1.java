661c5b9507a4cb07e58d02c5d7f981b7e87fbd9c
Ernqi <S10243067@connect.np.edu.sg>
2023-04-19 00:29:50 +0800

    again

--- Question1.java
+++ Question1.java
@@ -1,15 +1,15 @@
-
 public class Question1
 {
   public static void main(String[] args)
   {
-    
+    /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
     System.out.println("Hello World!");
-                                              
   }
-
 }
-
 
 
  
